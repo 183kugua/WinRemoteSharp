c4e3aa88a87cbef8e146edf3bff3b2b38dc4e4dd
183kugua <18312626688@163.com>
2026-08-07 14:18:03 +0800

    Update AgentClient.cs

--- Core/AgentClient.cs
+++ Core/AgentClient.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Net.WebSockets;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
